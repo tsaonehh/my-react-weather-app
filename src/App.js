808e55e59ecd497d7c74a77d391aef3c38679e81
Tsaone <tsaoneb3@gmail.com>
2023-08-05 11:41:37 +0200

    Amended links in footer

--- src/App.js
+++ src/App.js
@@ -7,13 +7,13 @@ export default function App() {
       <h1>Weather App</h1>
       <footer>
         This project was coded by{" "}
-        <a href="https://meek-cendol-e5e18a.netlify.app" target="_blank">
+        <a href="https://meek-cendol-e5e18a.netlify.app" target="_blank" rel="nonreferrer">
           Tsaone W Bejile
         </a>
         {" "}and is{" "}
         <a
           href="https://github.com/tsaonehh/my-react-weather-app"
-          target="_blank"
+          target="_blank" rel="nonreferrer"
         >
           open-sourced on GitHub
         </a>
