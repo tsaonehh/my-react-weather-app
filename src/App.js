0bc7e34900bffada034b64c536fd2d1bbf4b8f93
Tsaone <tsaoneb3@gmail.com>
2023-08-05 11:02:41 +0200

    Ammended footer

--- src/App.js
+++ src/App.js
@@ -2,16 +2,23 @@
 import './App.css';
 
 export default function App() {
-  return(
+  return (
     <div className="App">
       <h1>Weather App</h1>
       <footer>
-        This project was coded by <a href="https://meek-cendol-e5e18a.netlify.app" target="_blank">Tsaone W Bejile</a> and is{" "}
-        <a href="https://github.com/tsaonehh/my-react-weather-app" target="_blank">
+        This project was coded by{" "}
+        <a href="https://meek-cendol-e5e18a.netlify.app" target="_blank">
+          Tsaone W Bejile
+        </a>
+        {" "}and is{" "}
+        <a
+          href="https://github.com/tsaonehh/my-react-weather-app"
+          target="_blank"
+        >
           open-sourced on GitHub
         </a>
       </footer>
     </div>
-    );
+  );
 }
 
