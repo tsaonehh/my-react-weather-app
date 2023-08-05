c438c3d80a38ed3e8db96ba00d526e18b708b99f
Tsaone <tsaoneb3@gmail.com>
2023-08-05 10:48:02 +0200

    Added GitHub REPO Url

--- src/App.js
+++ src/App.js
@@ -4,7 +4,13 @@ import './App.css';
 export default function App() {
   return(
     <div className="App">
-      Weather App
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by <a href="https://meek-cendol-e5e18a.netlify.app" target="_blank">Tsaone W Bejile</a> and is{" "}
+        <a href="https://github.com/tsaonehh/my-react-weather-app" target="_blank">
+          open-sourced on GitHub
+        </a>
+      </footer>
     </div>
     );
 }
